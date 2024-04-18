c6fce3afc5b8241efc7b33df4930d0e9250e6179
Riaz9655 <mriaz875@gmail.com>
2024-04-18 14:17:19 +0500

    initial
    initial comit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
+
+import 'home_screen.dart';
+import 'models/notes_model.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  var directory = await getApplicationDocumentsDirectory() ;
+  Hive.init(directory.path);
+
+  Hive.registerAdapter(NotesModelAdapter()) ;
+  await Hive.openBox<NotesModel>('notes');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+        appBarTheme: AppBarTheme(color: Colors.teal,foregroundColor: Colors.white,centerTitle: true)
+      ),
+      home:  HomeScreen(),
+    );
+  }
+}
+
